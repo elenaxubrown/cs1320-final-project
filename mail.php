87ad48ca6ff6b8b27d71c3338a455a662ac56f0c
Yiquan Xu <yiquan_xu@brown.edu>
2019-04-23 18:00:16 -0400

    Add emails function

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
-<?php
-    $to = 'demo@spondonit.com';
+<!-- <?php
+    $to = '854832347@qq.com';
     $firstname = $_POST["fname"];
     $email= $_POST["email"];
     $text= $_POST["message"];
@@ -21,11 +21,60 @@
         
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
-    }
+    $retval =  mail ($to, $email,$message,$headers);
+
+    // if (@mail($to, $email, $message, $headers))
+    // {
+    //     echo 'The message has been sent.';
+    // }else{
+    //     echo 'failed';
+    // }
+
+    if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+
+?>
+ -->
+
+
+ <?php
+    $to = 'elenaxu517@gmail.com';
+    
+    $subject= $_POST["subject"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    // $text= $_POST["message"];
+    // $phone= $_POST["phone"];
+    
+
+
+    // $headers = 'MIME-Version: 1.0' . "\r\n";
+    // $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    // $message ="THis is the message";
+
+    $header = "From: " . $email . "\r\n"; // Sender's E-mail
+         // $header .= "Cc:afgh@somedomain.com \r\n";
+          $header .= "MIME-Version: 1.0\r\n";
+          $header .= "Content-type: text/html\r\n";
+
+    $retval =  mail ($to, $subject,$message,$header);
+
+    // if (@mail($to, $email, $message, $headers))
+    // {
+    //     echo 'The message has been sent.';
+    // }else{
+    //     echo 'failed';
+    // }
+
+    if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
 
 ?>
